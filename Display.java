93afa7f726d0b3de270698fec0f6b95acd52b08a
NagarajuJN <nagaraju1998jn@gmail.com>
2023-06-26 13:49:37 +0530

    for loop is added

--- Display.java
+++ Display.java
@@ -2,6 +2,9 @@ class Display
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		for(int i =0 ; i<5 ; i++)
+		{
+			System.out.println("Pentagonspace");
 	}
+}
 }
